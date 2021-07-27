6036017a15fdf5d2e1bb4ce304a55eda3f325b90
sshabaj1 <senadshabaj73@gmail.com>
2021-07-27 18:16:25 +0200

    second commit

--- libpro/forms.py
+++ libpro/forms.py
@@ -1,16 +1,21 @@
 from django import forms
 from django.forms import widgets
-from .models import Post
+from .models import Post, Category
+
+choices = Category.objects.all().values_list('name','name')
+
+
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields =('title', 'title_tag', 'author', 'body')
+        fields =('title', 'title_tag', 'author', 'category', 'body')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Post Title'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Post Title Tag'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices = choices,attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Whats in your mind'}),
         }
 
--- libpro/models.py
+++ libpro/models.py
@@ -3,12 +3,25 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('home')
+
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     post_date = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=255, default = 'Personal')
     
 
 
