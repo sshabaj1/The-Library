f6b4bf2bd1e424cffe1537d32ec684f03cd69392
sshabaj1 <senadshabaj73@gmail.com>
2021-07-28 12:29:27 +0200

    user restrictions #2

--- libpro/forms.py
+++ libpro/forms.py
@@ -17,7 +17,8 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Post Title Tag'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control','value':'', 'id': 'identifier', 'type':'hidden'}),
+            # 'author': forms.Select(attrs={'class': 'form-control'}),
             'category': forms.Select(choices = choice_list,attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Whats in your mind'}),
         }
