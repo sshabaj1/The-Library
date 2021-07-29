5d7d016eec6a818725f5ea0a3cf2061950bdf2fe
sshabaj1 <senadshabaj73@gmail.com>
2021-07-29 11:57:39 +0200

    test

--- libpro/urls.py
+++ libpro/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('add_category/',AddCategoryView.as_view(), name = 'add_category'),
     path('category/<str:cats>/', CategoryView, name = 'category'),
     path('category-list/', CategoryListView, name = 'category-list'),
-    path('like/<int:pk>', LikeView, name = 'like_post'),
+    path('like/<int:pk>/', LikeView, name = 'like_post'),
 
 
 ]
--- libpro/views.py
+++ libpro/views.py
@@ -1,6 +1,7 @@
 from libpro.models import Post
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views import generic
 from .models import Post, Category
 from .forms import PostForm, EditForm
 from django.urls import reverse_lazy, reverse
@@ -10,49 +11,49 @@ from django.http import HttpResponseRedirect
 #     return render(request, 'home.html', {})
 
 
-
-def LikeView(request, pk=id):
+def LikeView(request, pk):
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
     post.likes.add(request.user)
     return HttpResponseRedirect(reverse('post-details', args=[str(pk)]))
-    
+
+
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
     ordering = ['-post_date']
 
-
-    def get_context_data(self,*args,**kwargs):
+    def get_context_data(self, *args, **kwargs):
         cat_menu = Category.objects.all()
-        context = super(HomeView,self).get_context_data(*args,**kwargs)
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
         context['cat_menu'] = cat_menu
         return context
 
+
 def CategoryListView(request):
     cat_menu_list = Category.objects.all()
-    return render(request, 'category_list.html', {'cat_menu_list':cat_menu_list})
+    return render(request, 'category_list.html', {'cat_menu_list': cat_menu_list})
+
 
 def CategoryView(request, cats):
     category_posts = Post.objects.filter(category=cats)
-    return render(request, 'categories.html', {'cats':cats.replace('-',' ').title(), 'category_posts':category_posts})
+    return render(request, 'categories.html', {'cats': cats.replace('-', ' ').title(), 'category_posts': category_posts})
 
 
 class PostDetailsView(DetailView):
     model = Post
     template_name = 'post_details.html'
 
-    def get_context_data(self,*args,**kwargs):
+    def get_context_data(self, *args, **kwargs):
         cat_menu = Category.objects.all()
-        context = super(PostDetailsView,self).get_context_data(*args,**kwargs)
-        
-        
+        context = super(PostDetailsView, self).get_context_data(
+            *args, **kwargs)
+
         stuff = get_object_or_404(Post, id=self.kwargs['pk'])
         total_likes = stuff.total_likes()
         context['cat_menu'] = cat_menu
         context['total_likes'] = total_likes
         return context
-
 
 
 class AddPostView(CreateView):
@@ -66,10 +67,12 @@ class AddCategoryView(CreateView):
     template_name = 'add_category.html'
     fields = '__all__'
 
+
 class UpdatePostView(UpdateView):
     model = Post
     form_class = EditForm
     template_name = 'update_post.html'
+
 
 class DeletePostView(DeleteView):
     model = Post
