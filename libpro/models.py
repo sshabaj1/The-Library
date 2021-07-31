142d96772d3562f2324e02fb4b6d89110f04c11d
sshabaj1 <senadshabaj73@gmail.com>
2021-07-31 14:22:04 +0200

    Like button fix

--- libpro/models.py
+++ libpro/models.py
@@ -22,7 +22,7 @@ class Post(models.Model):
     body = models.TextField()
     post_date = models.DateTimeField(auto_now_add=True)
     category = models.CharField(max_length=255, default = 'Personal')
-    likes = models.ManyToManyField(User, related_name='blog_post')
+    likes = models.ManyToManyField(User, related_name='blog_posts')
 
     def total_likes(self):
         return self.likes.count()
--- libpro/views.py
+++ libpro/views.py
@@ -1,6 +1,12 @@
 from libpro.models import Post
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from django.views import generic
 from .models import Post, Category
 from .forms import PostForm, EditForm
@@ -12,69 +18,80 @@ from django.http import HttpResponseRedirect
 
 
 def LikeView(request, pk):
-    post = get_object_or_404(Post, id=request.POST.get('post_id'))
-    post.likes.add(request.user)
+    post = get_object_or_404(Post, id=pk)
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+    
+        post.likes.add(request.user)
+        liked = True
+
     return HttpResponseRedirect(reverse('post-details', args=[str(pk)]))
 
 
 
 class HomeView(ListView):
     model = Post
-    template_name = 'home.html'
-    ordering = ['-post_date']
+    template_name = "home.html"
+    ordering = ["-post_date"]
 
     def get_context_data(self, *args, **kwargs):
         cat_menu = Category.objects.all()
         context = super(HomeView, self).get_context_data(*args, **kwargs)
-        context['cat_menu'] = cat_menu
+        context["cat_menu"] = cat_menu
         return context
 
 
 def CategoryListView(request):
     cat_menu_list = Category.objects.all()
-    return render(request, 'category_list.html', {'cat_menu_list': cat_menu_list})
+    return render(request, "category_list.html", {"cat_menu_list": cat_menu_list})
 
 
 def CategoryView(request, cats):
     category_posts = Post.objects.filter(category=cats)
-    return render(request, 'categories.html', {'cats': cats.replace('-', ' ').title(), 'category_posts': category_posts})
+    return render(
+        request,
+        "categories.html",
+        {"cats": cats.replace("-", " ").title(), "category_posts": category_posts},
+    )
 
 
 class PostDetailsView(DetailView):
     model = Post
-    template_name = 'post_details.html'
+    template_name = "post_details.html"
 
     def get_context_data(self, *args, **kwargs):
         cat_menu = Category.objects.all()
-        context = super(PostDetailsView, self).get_context_data(
-            *args, **kwargs)
+        context = super(PostDetailsView, self).get_context_data(*args, **kwargs)
 
-        stuff = get_object_or_404(Post, id=self.kwargs['pk'])
+        stuff = get_object_or_404(Post, id=self.kwargs["pk"])
         total_likes = stuff.total_likes()
-        context['cat_menu'] = cat_menu
-        context['total_likes'] = total_likes
+        context["cat_menu"] = cat_menu
+        context["total_likes"] = total_likes
         return context
 
 
 class AddPostView(CreateView):
     model = Post
     form_class = PostForm
-    template_name = 'add_post.html'
+    template_name = "add_post.html"
 
 
 class AddCategoryView(CreateView):
     model = Category
-    template_name = 'add_category.html'
-    fields = '__all__'
+    template_name = "add_category.html"
+    fields = "__all__"
 
 
 class UpdatePostView(UpdateView):
     model = Post
     form_class = EditForm
-    template_name = 'update_post.html'
+    template_name = "update_post.html"
 
 
 class DeletePostView(DeleteView):
     model = Post
-    template_name = 'delete_post.html'
-    success_url = reverse_lazy('home')
+    template_name = "delete_post.html"
+    success_url = reverse_lazy("home")
