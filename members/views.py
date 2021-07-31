44efc933ce980ee5862eec6b5ce000b79b9f8a4c
sshabaj1 <senadshabaj73@gmail.com>
2021-07-31 20:00:45 +0200

    sign up form updated

--- members/views.py
+++ members/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from .forms import SingUpForm
 
 # Create your views here.
 
 
 class UserRegisterView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SingUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
